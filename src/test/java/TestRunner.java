4b5f72b52b666bd697744e4e3e257defe6c11fb0
artur-arutsiunian <artur.arutsiunian93@gmail.com>
2023-02-18 12:28:04 +0100

    new pom22

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -3,7 +3,7 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.runner.RunWith;
 
 @RunWith(JUnitPlatform.class)
-@SelectClasses({APITest.class})
+@SelectClasses({})
 
 public class TestRunner {
     }
