abc39ac6849c21928fd063379ee62ea0fb781fb9
artur-arutsiunian <artur.arutsiunian93@gmail.com>
2023-02-18 11:54:35 +0100

    new pom13

--- src/test/java/APITest.java
+++ src/test/java/APITest.java
@@ -1,41 +1,49 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 
+//@Execution(ExecutionMode.CONCURRENT)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 
     public class APITest extends TestRunner {
 
         private static final RestService REST_SERVICE1 = new RestService();
 
+        @Order(1)
         @Test
         public void checkGetUsers(){
             Assertions.assertEquals("male", REST_SERVICE1.getUsers().getPlayers().get(0).getGender(), "Quantity of users aren't correct");
         }
 
+        @Order(2)
         @Test
         public void checkGetOneUser(){
             Assertions.assertEquals("supervisor", REST_SERVICE1.getUserById(1).getLogin(), "'Login' isn't correct");
         }
 
+        @Order(3)
         @Test
         public void checkCreateUser(){
             Assertions.assertEquals("test126", REST_SERVICE1.createUser().getLogin(), "User with such login isn't created");
         }
 
+        @Order(4)
         @Test
         public void checkUserIsNotCreated() {
             Assertions.assertEquals(400, REST_SERVICE1.createUserWithWrongAge(), "Status code isn't correct");
         }
 
+        @Order(5)
         @Test
         public void checkEditUser(){
             Assertions.assertEquals("female", REST_SERVICE1.editUser("female").getGender(), "Gender isn't correct");
         }
 
+        @Order(6)
         @Test
         public void checkDeleteUser(){
-            Assertions.assertEquals(204, REST_SERVICE1.deleteUser(1483982699), "User isn't deleted");
-        }
+        Assertions.assertEquals(204, REST_SERVICE1.deleteUser(1483982699), "User isn't deleted");
+    }
+
+
 }
--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -3,7 +3,7 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.runner.RunWith;
 
 @RunWith(JUnitPlatform.class)
-@SelectClasses(APITest.class)
+@SelectClasses({APITest.class})
 
 public class TestRunner {
     }
